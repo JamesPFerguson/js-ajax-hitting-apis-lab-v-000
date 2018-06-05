f685c6e39d2942385e8ea1e85b7f6440d2b8665e
James Ferguson <ncjamesferguson@gmail.com>
2018-06-05 15:37:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function getRepositories() {
-
+  let username = document.getElementById('username').value
+  let url = 'https://api.github.com/users' + username + '/repos'
+  let req = new XMLHttpRequest();
+  req.addEventListener("load", displayRepositories);
+  req.open("GET", url);
+  req.send();
 }
