e96b649d12d2f080e76f64934c81c39c4231775d
James Ferguson <ncjamesferguson@gmail.com>
2018-06-05 16:20:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ function getRepositories() {
   req.open("GET", url);
   req.send();
 }
+
+function displayRepositories() {
+  let repos = JSON.parse(this.responseText);
+  let repoList = "<ul>" + repos.map(repo => {
+    let username = repo.owner.login
+    let reponame = repo.name
+    return('<li>
+    <h2>${repo.name}</h2>
+            <a href="${repo.html_url}">${repo.html_url}</a><br>
+            <a href="#" ${repoName} ${username} onclick="getCommits(this)">Get Commits</a><br>
+          <a href="#" ${reponame} ${reponame} onclick="getBranches(this)">Get Branches</a></li>
+            
+    </li>
+    ')
+  })
+}
